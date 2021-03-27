edc62ff66e6d02c8f12c5d0b5af9f6b6802f32c6
Chris Parsons <mail@chrisparsons.dev>
2021-03-27 17:38:07 +0000

    Add simple table format

---
+++ src/components/ResultsTable.tsx
@@ -0,0 +1,23 @@
+const ResultsTable = () => {
+  return (
+    <table>
+      <tr>
+        <th>Team</th>
+        <th>Week 1</th>
+        <th>Week 2</th>
+        <th>Week 3</th>
+        <th>Week 4</th>
+      </tr>
+      <tr>
+        <td>Doggos</td>
+        <td>1</td>
+        <td>1234</td>
+        <td>4312</td>
+        <td>124325</td>
+        <td>784327</td>
+      </tr>
+    </table>
+  )
+}
+
+export default ResultsTable;
