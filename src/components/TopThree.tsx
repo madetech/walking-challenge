20a6e6de4add40f62ae81718c46f2b8c0813414a
Christopher Parsons <chris.p@madetech.com>
2021-04-01 00:15:31 +0100

    Update TopThree to only show values when distance has been entered

--- src/components/TopThree.tsx
+++ src/components/TopThree.tsx
@@ -7,26 +7,72 @@ interface TopThreeProps {
 }
 
 const TopThree = ({ tableValues }: TopThreeProps) => {
+  const firstPlace = tableValues[0]?.fields;
+  const secondPlace = tableValues[1]?.fields;
+  const thirdPlace = tableValues[2]?.fields;
+
+  const renderFirstPlace = () => {
+    if (firstPlace?.total === 0) {
+      return (
+        <div id="first">
+          🥇 1st Place: ???
+        </div>
+      )
+    }
+    return (
+      <div id="first">
+        🥇 1st Place:{" "}
+        <span className="team-name">{firstPlace["Team Name"]}</span>
+      </div>
+    );
+  };
+
+  const renderSecondPlace = () => {
+    if (secondPlace?.total === 0) {
+      return (
+        <div id="second">
+          🥈 2nd Place: ???
+        </div>
+      )
+    }
+    return (
+      <div id="second">
+        🥈 2nd Place:{" "}
+        <span className="team-name">{secondPlace["Team Name"]}</span>
+      </div>
+    );
+  };
+
+  const renderThirdPlace = () => {
+    if (thirdPlace?.total === 0) {
+      return (
+        <div id="third">
+          🥉 3rd Place: ???
+        </div>
+      )
+    }
+    return (
+      <div id="third">
+        🥉 3rd Place:{" "}
+        <span className="team-name">{thirdPlace["Team Name"]}</span>
+      </div>
+    );
+  };
+
   if (tableValues.length >= 3) {
     return (
       <div className="ranking">
         <Row>
           <Col>
-            <div id="first">
-              🥇 1st Place: <span className="team-name">{tableValues[0].fields["Team Name"]}</span>
-            </div>
+            {renderFirstPlace()}
           </Col>
         </Row>
         <Row>
           <Col>
-            <div id="second">
-              🥈 2nd Place: <span className="team-name">{tableValues[1].fields["Team Name"]}</span>
-            </div>
+            {renderSecondPlace()}
           </Col>
           <Col>
-            <div id="third">
-              🥉 3rd Place: <span className="team-name">{tableValues[2].fields["Team Name"]}</span>
-            </div>
+            {renderThirdPlace()}
           </Col>
         </Row>
       </div>
